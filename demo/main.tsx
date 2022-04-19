01bc2818f34e2ad56cf6322c8f22c7afb5f936cf
Alec <alec.minch@gmail.com>
2022-04-19 11:24:39 -0400

    make react a peerdep

--- demo/main.tsx
+++ demo/main.tsx
@@ -1,5 +1,5 @@
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { App } from './app';
 import './index.css';
 
-createRoot(document.getElementById('root') as HTMLElement).render(<App />);
+ReactDOM.render(<App />, document.getElementById('root'));
